3b8ca0b19ac8a6f14c0f44565f60ef0b0152c54d
Natalia Smirnova <Natalia_Smirnova@epam.com>
2018-08-24 15:16:46 +0300

    Fix controller position

--- layer0/OpenVRStereo.cpp
+++ layer0/OpenVRStereo.cpp
@@ -530,7 +530,7 @@ static void ConvertSteamVRMatrixToGLMat(float const* srcMat34, float *dstMat44)
   dst[0][0] = src[0][0]; dst[0][1] = src[1][0]; dst[0][2] = src[2][0]; dst[0][3] = 0.0f;
   dst[1][0] = src[0][1]; dst[1][1] = src[1][1]; dst[1][2] = src[2][1]; dst[1][3] = 0.0f;
   dst[2][0] = src[0][2]; dst[2][1] = src[1][2]; dst[2][2] = src[2][2]; dst[2][3] = 0.0f;
-  dst[3][0] = src[0][3]; dst[3][1] = src[1][3]; dst[3][2] = src[3][2]; dst[3][3] = 1.0f;
+  dst[3][0] = src[0][3]; dst[3][1] = src[1][3]; dst[3][2] = src[2][3]; dst[3][3] = 1.0f;
 }
 
 
